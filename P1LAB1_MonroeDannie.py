d15fe9a43b0e5d2faa5d3ae873191bbf8c312cd3
Monroed1885 <150168852+Monroed1885@users.noreply.github.com>
2023-11-07 11:21:53 -0500

    First upload

---
+++ P1LAB1_MonroeDannie.py
@@ -0,0 +1,9 @@
+#Dannie Monroe
+#20231107
+#Basic Input and Output
+
+#Get Input from user
+name = input("Please enter your name: ")
+
+#display output to the user
+print("Hello", name, "welcome to cti-110!")
